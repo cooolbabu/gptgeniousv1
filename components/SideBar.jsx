3df15c3075feb0528634df772bc9bfa7e2a59cc4
Sreenivas Angara <cooolbabu@yahoo.com>
2024-02-27 21:12:12 -0600

    added tags and punchlines

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -1,15 +1,19 @@
 import MemberProfile from "./MemberProfile";
 import NavLinks from "./NavLinks";
+import Punchines from "./Punchines";
 import SidebarHeader from "./SidebarHeader";
+import Tagsarea from "./Tagsarea";
 
 function SideBar() {
   return (
-    <div className="px-4 w-80 min-h-full bg-base-300 py-12 grid grid-rows-[auto,1fr,auto] ">
+    <div className="px-4 w-80 min-h-full bg-base-300 py-12 grid grid-rows-[auto, 1fr, 1fr, 1fr,auto] ">
       {/* first row */}
       <SidebarHeader />
       {/* second row */}
       <NavLinks />
       {/* third row */}
+      <Tagsarea />
+      <Punchines />
       <MemberProfile />
     </div>
   );
