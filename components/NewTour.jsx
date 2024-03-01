b19c3264b44078a66d2e6f6f2a2ce5e41736e5af
Sreenivas Angara <cooolbabu@yahoo.com>
2024-03-01 14:58:57 -0600

    saving work

---
+++ components/NewTour.jsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import {
+  getExistingTour,
+  generateTourResponse,
+  createNewTour,
+} from "@/utils/actions";
+import TourInfo from "./TourInfo";
+
+function NewTour() {
+  const {
+    mutate,
+    isPending,
+    data: tour,
+  } = useMutation({
+    mutationFn: async (destination) => {
+      const newTour = await generateTourResponse(destination);
+      console.log("tour data from getExistingTour", newTour);
+      if (newTour) {
+        return newTour;
+      }
+      toast.error("No matching tour");
+
+      return null;
+    },
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const destination = Object.fromEntries(formData.entries());
+    //console.log(destination);
+    mutate(destination);
+  };
+
+  if (isPending) {
+    return <span className="loading loading-lg">Loading... Sunil</span>;
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="max-w-2xl">
+        <h2 className=" mb-4">Select your dream destination</h2>
+        <div className="join w-full">
+          <input
+            type="text"
+            className="input input-bordered join-item w-full"
+            placeholder="city"
+            name="city"
+            required
+          />
+          <input
+            type="text"
+            className="input input-bordered join-item w-full"
+            placeholder="country"
+            name="country"
+            required
+          />
+          <button className="btn btn-primary join-item" type="submit">
+            generate tour
+          </button>
+        </div>
+      </form>
+      <div className="mt-16">{tour ? <TourInfo tour={tour} /> : null}</div>
+    </>
+  );
+}
+export default NewTour;
--- utils/actions.js
+++ utils/actions.js
@@ -28,6 +28,56 @@ export const generateChatResponse = async (chatMessages) => {
   }
 };
 
+export async function getExistingTour({ city, country }) {
+  console.log("getExistingTour invoked");
+  return null;
+}
+
+export async function generateTourResponse({ city, country }) {
+  console.log("generateTourResponse invoked", city, country);
+
+  const query = `Find a exact ${city} in this exact ${country}.
+    If ${city} and ${country} exist, create a list of things families can do in this ${city},${country}. 
+    Once you have a list, create a one-day tour. Response should be  in the following JSON format: 
+    {
+      "tour": {
+        "city": "${city}",
+        "country": "${country}",
+        "title": "title of the tour",
+        "description": "short description of the city and tour",
+        "stops": ["short paragraph on the stop 1 ", "short paragraph on the stop 2","short paragraph on the stop 3"]
+      }
+    }
+    "stops" property should include only three stops.
+    If you can't find info on exact ${city}, or ${city} does not exist, or it's population is less than 1, 
+    or it is not located in the following ${country},   return { "tour": null }, with no additional characters.`;
+
+  try {
+    const response = await openai.chat.completions.create({
+      messages: [
+        { role: "system", content: "You are a tour guide" },
+        { role: "user", content: query },
+      ],
+      model: "gpt-3.5-turbo",
+      temperature: 0.8,
+    });
+    const tourData = JSON.parse(response.choices[0].message.content);
+    if (!tourData.tour) {
+      return null;
+    }
+    return tourData.tour;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+  return null;
+}
+
+export async function createNewTour(tour) {
+  console.log("createNewTour invoked");
+  return null;
+}
+
 // // This does not save Chat History
 // //
 // export const generateChatResponse = async (chatMessage) => {
